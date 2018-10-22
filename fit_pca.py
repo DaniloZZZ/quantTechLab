1ca36138199d09cce6dd363268de634622087d64
Danil Lykov <lkv97dn@gmail.com>
2018-10-23 00:53:33 +0300

    image recogn working

--- fit_pca.py
+++ fit_pca.py
@@ -1,9 +1,67 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 from PIL import Image
+from sklearn.decomposition import PCA as PCA_sk
 import numpy as np
 
+def pca_sklearn(data,n_components=10):
+    """
+    Perform PCA decomosition
+    Parameters
+    ----------
+    data: iterable
+        data to get images from, numpy array
+    n_components: int
+        number of remaining components
+
+    Returns
+    --------
+    (sklearn.PCA, np.array)
+        PCA fitted transformer and transformed data
+    """
+    pca = PCA_sk(n_components=n_components, random_state=42)
+    X = [d.flatten() for d in data]
+    transformed = pca.fit_transform(X)
+    return pca,transformed
+
+def get_pca_sk_transformer(data,n_components=10):
+    """
+    Get Generator that transforms pca
+    Parameters
+    ----------
+    data: iterable
+        data to get images from, numpy array
+    n_components: int
+        number of remaining components
+
+    Returns
+    --------
+    (generator, np.array)
+        generator of transformed images and transformed train data
+    """
+    pca,transformed = pca_sklearn(data,n_components=n_components)
+    def generator(data):
+        for image in data:
+            x = image.flatten().reshape(1,-1)
+            yield pca.transform(x)
+    return generator, transformed
+
 def splitter(data,parts=6):
+    """
+    a generator of image parts, every image gets splitted
+    to `parts`**2 parts. 
+    Parameters
+    ----------
+    data: iterable
+        data to get images from
+    parts: int
+        specify ratio of inital image and small
+
+    yields
+    --------
+    np.array
+        parts of image
+    """
     for img in data:
         w,h = img.width,img.height
         dw,dh = w//parts, h//parts
@@ -27,22 +85,23 @@ def merger(patches,parts=6):
         idx+=1
 
 def pca_core(X,num_components):
-    X=np.array([l-np.mean(l) for l in X]) # Вычитаем среднее
-    U,s,V=np.linalg.svd(X)                # Находим собственные вектора
-    eps=np.sort(s)[-num_components]       # пороговое собственное значение (служебная строчка) 
+    X=np.array([l-np.mean(l) for l in X]) 
+    U,s,V=np.linalg.svd(X)                
+    eps=np.sort(s)[-num_components]       
     E = np.array([vec for val,vec in zip(s,V)
-                  if val>eps])            # берем только важные вектора   (с большими собственными значениями)
-    X_=np.dot(E,X.T).T                    # Преобразуем данные
+                  if val>eps])            
+    X_=np.dot(E,X.T).T                    
     return X_,E,s
 
+
 def get_PCA_gen(dset,factor=0.5,parts=6,num_components=10):
-	patches= list(splitter( resizer(dset, factor=factor), parts=parts ))
-	X = np.array([i[:,:,1].flatten() for i in patches])
-	transformed, operator, s = pca_core(X, num_components)
-	def generator(data):
-		for image in data:
-			yield np.dot(E,image)
-	return generator, transformed
+    patches= list(splitter( resizer(dset, factor=factor), parts=parts ))
+    X = np.array([i[:,:,1].flatten() for i in patches])
+    transformed, operator, s = pca_core(X, num_components)
+    def generator(data):
+        for image in data:
+            yield np.dot(E,image)
+    return generator, transformed
 
 def resizer(images,factor=0.5):
     for i in images:
