aaf1d64d8637816c0c326ef5744d34de40d9fec0
DaniloZZZ <lkv97dn@gmail.com>
2019-04-03 01:28:14 +0300

    fixed device fucntion

--- Fit/np_fun.py
+++ Fit/np_fun.py
@@ -6,6 +6,7 @@ r = np.sqrt(1-T)
 a = np.sqrt(T*(1-T))
 b = np.sqrt(1-T)
 c = np.sqrt(T)
+a = b = c = 1
 
 def a01(al=np.pi, thet=0):
     return np.exp(1j*thet)*\
@@ -58,32 +59,30 @@ def device_function(x,phi):
     return np.abs(np.square(prob_part(*x,phi=phi)))
 
 def prob_part(*x,phi=0.1,alp = 0.125):
-    a1,al1,al2,al3,al4,ala,alb,th1,th2,th3,th4,thb = 0.125,0,np.pi,0,np.pi,np.pi,np.pi,0,0,0,0,0
-    psi=1.33
+    a1 = 0.
+    asq2 = np.arctan(np.sqrt(2))
+    g1,g2,g3 = np.sqrt(1-0.15), np.sqrt( 1-0.2), np.sqrt(1-0.28)
     op = np.diag([1,1,1])
     matr = [
         #np.array([0,0,1]),
-        a12(al4,th4),
-        ph(0.,np.arctan(1.41)-2*psi+al4-th3-th2-th1-al1+al2+alb+ x[3],0.),
+        a12(np.pi/2, 0),
+        ph(0., np.pi/2 + asq2 + x[3], 0),
         refl1(a1),
 
-        a01(al3,th3),
-        ph(-2*np.arctan(1.41)+al3-al2+np.pi+x[2],0,0),
+        a01(np.pi/2, 0),
+        ph(np.pi - 2*asq2 + x[2], 0, 0),
         refl0(a1),
-        ph(psi,psi,0),
 
-        a01(al2,th2),
-        ph(0,-al2+np.pi/2.-th1-alb+x[1],0),
+        a01(np.pi/2, 0),
+        ph(0, x[1], 0),
         refl1(a1),
-        ph(ala+thb+psi,psi,0),
 
-        a12(al1,th1),
-        ph(0,0,-al1-psi+ala+thb+alb+x[0]),
+        a12(np.pi/2, 0),
+        ph(0, 0, np.pi*3/2 + x[0]),
         refl2(a1),
-        ph(0,ala+thb+alb,psi),
-        refl2(g3),
-        refl2(g2),
-        refl1(g1),
+        #refl2(g3),
+        #refl2(g2),
+        #refl1(g1),
 
         ph2(phi),
     ]
