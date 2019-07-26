4f5fbc5c11481c0e7f87ee12cbe27cb41415d020
maksimrm <maximrm@icloud.com>
2019-07-27 02:16:19 +0300

    train_test + pca dataset

--- analysis/ImageDataset.py
+++ analysis/ImageDataset.py
@@ -5,11 +5,52 @@ import numpy as np
 from pathlib import Path
 from torch.utils.data import Dataset
 import imageio
+import random
+
+
+
+
+class train_test_split():
+    def __init__(self, folder,
+                 filtered = None, transform = None, pca = None):
+        """
+        pca: get module PCA(N_components).
+        
+        
+        """
+        self.transform = transform
+        self.filtered = filtered
+        self.pca = pca
+        self.folder = Path(folder)
+        self.names = os.listdir(self.folder)[:-1]
+        need = lambda x: ".png" in x
+        self.names = list(filter(need, self.names))
+        self.names.sort(key= lambda x: int(x.split("_")[0]))
+    
+    def get(self, start = 0, end = 1, shuffle = False, test_size = 0.2):
+        self.names = self.names[int(start*len(self.names)):int(end*len(self.names))]  
+        if shuffle:
+            random.shuflle(self.names)
+        train_names = self.names[:int((1-test_size)*len(self.names))]
+        test_names = self.names[int((1-test_size)*len(self.names)):]
+        train_dataset = FolderImageDataset(self.folder, filtered = self.filtered, 
+                                          transform = self.transform, names = train_names) 
+        test_dataset = FolderImageDataset(self.folder, filtered = self.filtered, 
+                                          transform = self.transform, names = test_names)
+        if self.pca:
+                self.pca.fit(self.data_transform(train_dataset))
+                train_dataset.images = self.pca.transform(self.data_transform(train_dataset))
+                test_dataset.images = self.pca.transform(self.data_transform(test_dataset))
+        return train_dataset, test_dataset
+    
+    
+    def data_transform(self, data):
+        return np.array(data.images).reshape(len(data), -1)
 
 
 class FolderImageDataset(Dataset):
-    def __init__(self, folder, start = 0, end = 1,
-                 filtered = None, transform = None):
+    def __init__(self, folder,
+                 filtered = None, transform = None, names = None):
         """
         Reads images from folder and optionally applies a transform to them.
         Filter parameter takes target value and returns bool
@@ -27,25 +68,31 @@ class FolderImageDataset(Dataset):
             float ->  bool. filters the dataset based on target
         """
         self.folder = Path(folder)
-        self.names = os.listdir(self.folder)[:-1]
-        
+        if names:
+            self.names = names
+        else:
+            
+            
+            self.names = os.listdir(self.folder)[:-1]
+            need = lambda x: ".png" in x
+            self.names = list(filter(need, self.names))
         #! TODO: adapt to new parameter interface
-        size = end-start
-        start = end==1
-        #---------
-        self.names = self.names[:int(0.5*(len(self.names)))]
+#         size = end-start
+#         start = end==1
+#         #---------
+#         #self.names = self.names[:int(0.5*(len(self.names)))]
         
-        if start:
-            self.names = self.names[:int(size*(len(self.names)))]
-        else: 
-            self.names = self.names[int((1-size)*(len(self.names))): ]
-        self.names.sort(key= lambda x: int(x.split("_")[0]))
+#         if start:
+#             self.names = self.names[:int(size*(len(self.names)))]
+#         else: 
+#             self.names = self.names[int((1-size)*(len(self.names))): ]
+#         self.names.sort(key= lambda x: int(x.split("_")[0]))
         self.transform = transform
         self.images = []
         self.target = []
-        for idx in range(len(self.names)):
-            self.images.append(np.array(imageio.imread(str(self.folder/self.names[idx]))))
-            self.target.append(float(self.names[idx].split("_")[1]))
+        for item in self.names:
+            self.images.append(np.array(imageio.imread(str(self.folder/item))))
+            self.target.append(float(item.split("_")[1]))
         self.images = np.array(self.images)
         self.target = np.array(self.target)
         
@@ -56,10 +103,16 @@ class FolderImageDataset(Dataset):
         
         
     def __getitem__(self, idx):
+        
         np_img = self.images[idx]
         y_true = self.target[idx]
+    
         if self.transform:
             np_img = self.transform(np_img)
+            y_true = torch.tensor(y_true, dtype=torch.float)
+            y_true = y_true.view(-1,)
+        else:
+            np_img = torch.tensor(np_img)
             y_true = torch.tensor(y_true, dtype=torch.float)
             y_true = y_true.view(-1,)
         return np_img, y_true
